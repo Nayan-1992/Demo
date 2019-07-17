834f4bd82f000cd29266ba5e82f016f87f3280c1
Nayan-1992 <48062037+Nayan-1992@users.noreply.github.com>
2019-07-17 15:04:29 +0530

    Update SendNotification.sh

--- SendNotification.sh
+++ SendNotification.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 if [ "$CODEBUILD_BUILD_SUCCEEDING" == "0" ]
 then
-   #$Result=(aws codebuild batch-get-builds --ids dino-test-scheduler-build-project:$CODEBUILD_BUILD_ID)
    curl -X POST -H 'Content-type: application/json' --data '{"text":"The Build is Failed ! => $Result"}' https://outlook.office.com/webhook/31d5743a-558b-4187-a7b7-a7be2d91188e@c303d480-d4ed-471a-9ada-36303e8f28b8/IncomingWebhook/682662b4347a4454acb9741625d4e49f/c56795cc-50fa-49e8-a86f-433df405be8d   
 else
    curl -X POST -H 'Content-type: application/json' --data '{"text":"The Build is Successfull !"}' https://outlook.office.com/webhook/31d5743a-558b-4187-a7b7-a7be2d91188e@c303d480-d4ed-471a-9ada-36303e8f28b8/IncomingWebhook/682662b4347a4454acb9741625d4e49f/c56795cc-50fa-49e8-a86f-433df405be8d
